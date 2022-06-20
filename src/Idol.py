21530c4c0de5deb207be7c5b0ce1c8b7ccdfd35a
AoWangPhilly <aw3338@drexel.edu>
2022-06-20 13:16:53 -0400

    Get tweets

--- src/Idol.py
+++ src/Idol.py
@@ -1,11 +1,13 @@
+from typing import Optional
 import tweepy
 from config import API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+from dataclasses import dataclass
 
-
+@dataclass
 class Idol:
     ...
 
-def connect_to_twitter() -> tweepy.API:
+def connect_to_twitter() -> Optional[tweepy.API]:
     auth = tweepy.OAuthHandler(
         consumer_key=API_KEY, 
         consumer_secret=API_KEY_SECRET, 
@@ -14,16 +16,17 @@ def connect_to_twitter() -> tweepy.API:
     )
 
     api = tweepy.API(auth)
-    return api
+    try:
+        api.verify_credentials()
+        print("Authentication OK")
+        return api
+    except:
+        print("Error during authentication")
 
 if __name__ == "__main__":
 
     api = connect_to_twitter()
-    
-    try:
-        api.verify_credentials()
-        print("Authentication OK")
-    except:
-        print("Error during authentication")
-
-    user = api.get_user(screen_name='moricalliope')
+    mori_tweets = api.user_timeline(screen_name="moricalliope")
+    for tweet in mori_tweets:
+        if "SCHEDULE" in tweet.text:
+            print(tweet.entities["media"][0]["media_url"])
