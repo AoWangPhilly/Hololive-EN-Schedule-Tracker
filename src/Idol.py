5bc40f8088f3c6ddec681c9f50dc87915c246600
AoWangPhilly <aw3338@drexel.edu>
2022-06-20 15:31:29 -0400

    Add in idol twitter tags

--- src/Idol.py
+++ src/Idol.py
@@ -1,32 +1,49 @@
-from typing import Optional
-import tweepy
-from config import API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+
 from dataclasses import dataclass
+from config import connect_to_twitter
+
+HOLOLIVE_EN_MYTH_TAGS = [
+    "moricalliope",
+    "takanashikiara",
+    "ninomaeinanis",
+    "gawrgura",
+    "watsonameliaEN",
+]
+
+HOLOLIVE_EN_VSINGER_TAGS = [
+    "irys_en"
+]
+
+HOLOLIVE_EN_COUNCIL_TAGS = [
+    "tsukumosana",
+    "ceresfauna",
+    "ourokronii",
+    "nanashimumei_en",
+    "hakosbaelz"
+]
+
 
 @dataclass
 class Idol:
-    ...
-
-def connect_to_twitter() -> Optional[tweepy.API]:
-    auth = tweepy.OAuthHandler(
-        consumer_key=API_KEY, 
-        consumer_secret=API_KEY_SECRET, 
-        access_token=ACCESS_TOKEN,
-        access_token_secret=ACCESS_TOKEN_SECRET
-    )
-
-    api = tweepy.API(auth)
-    try:
-        api.verify_credentials()
-        print("Authentication OK")
-        return api
-    except:
-        print("Error during authentication")
+    twitter_tag: str
 
 if __name__ == "__main__":
-
+    
     api = connect_to_twitter()
-    mori_tweets = api.user_timeline(screen_name="moricalliope")
-    for tweet in mori_tweets:
-        if "SCHEDULE" in tweet.text:
-            print(tweet.entities["media"][0]["media_url"])
+    # idol_tweets = api.user_timeline(
+    #     screen_name="watsonameliaEN", 
+    #     count=200,
+    #     include_rts = False,
+    #     tweet_mode = 'extended'
+    # )
+
+    # for tweet in idol_tweets:
+    #     # if "schedule" in tweet.full_text.lower():
+    #     # print(tweet.full_text)
+    #     try:
+    #         print(tweet.entities["media"][0]["media_url_https"])
+    #     except:
+    #         pass
+
+
+    
