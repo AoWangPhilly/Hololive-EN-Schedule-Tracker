09381ef4f5bcbe6e683334d40e3d175544650edb
AoWangPhilly <aw3338@drexel.edu>
2022-06-25 12:52:45 -0400

    Update models

--- src/models.py
+++ src/models.py
@@ -1,8 +1,19 @@
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.schema import ForeignKey, UniqueConstraint
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from database import Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.expression import null
+
+class Idol(Base):
+    __tablename__ = "idols"
+    __table_args__ = (UniqueConstraint("author_id"),)
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    author_id = Column(Integer, nullable=False)
+    username = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    icon_path = Column(String, nullable=False)
 
 class TwitterPost(Base):
     __tablename__ = "twitter_posts"
@@ -11,18 +22,9 @@ class TwitterPost(Base):
     twitter_post_id = Column(Integer, primary_key=True, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False)
     twitter_text = Column(String, nullable=False)
-    image_path = Column(String, nullable=False))
+    image_path = Column(String, nullable=False)
     youtube_link = Column(String, nullable=False)
 
     author_id = Column(Integer, ForeignKey(column="idols.author_id", ondelete="CASCADE"), nullable=False)
     author = relationship("Idol")
 
-
-class Idol(Base):
-    __tablename__ = "idols"
-
-    id = Column(Integer, primary_key=True, nullable=False)
-    author_id = Column(Integer, primary_key=True, nullable=False)
-    username = Column(String, nullable=False)
-    name = Column(String, nullable=False)
-    icon_path = Column(String, nullable=False)
