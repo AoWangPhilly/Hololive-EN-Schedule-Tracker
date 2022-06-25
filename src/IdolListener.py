4fc2037ab1c6f2b66c82eccaf3215b495d68d8be
AoWangPhilly <aw3338@drexel.edu>
2022-06-25 16:11:53 -0400

    Move listener to main

--- src/IdolListener.py
+++ src/IdolListener.py
@@ -1,8 +1,6 @@
 import tweepy as tw
-from decouple import config
-from util import generate_holo_en_rule
 from json import loads
-from pprint import pprint
+from util import store_twitter_post
 
 
 class IdolListener(tw.StreamingClient):
@@ -18,18 +16,4 @@ class IdolListener(tw.StreamingClient):
             print(tweet_data)
 
         elif not tweet_data["data"].get("referenced_tweets"):
-            pprint(tweet_data)
-            print()
-
-
-if __name__ == "__main__":
-    rule = generate_holo_en_rule()
-    streaming_client = IdolListener(config("BEARER_TOKEN"))
-    streaming_client.add_rules(tw.StreamRule(rule))
-
-    streaming_client.filter(
-        tweet_fields="id,text,author_id,created_at,entities,referenced_tweets",
-        expansions="author_id,attachments.media_keys",
-        media_fields="duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width,alt_text",
-        user_fields="username,name",
-    )
+            store_twitter_post(tweet_data)
