fecbf63b22a7106fc12d85844e00452d71a080e0
AoWangPhilly <aw3338@drexel.edu>
2022-06-25 13:00:16 -0400

    Test out data

--- src/main.py
+++ src/main.py
@@ -1,6 +1,51 @@
 from database import get_db, engine
 import models
-if __name__ == "__main__":
-    models.Base.metadata.create_all(engine)
+from pprint import pprint
 
-    get_db()
+if __name__ == "__main__":
+    # models.Base.metadata.create_all(engine)
+
+    # get_db()
+
+    a = {
+        "data": {
+            "attachments": {"media_keys": ["16_1540395710331228161"]},
+            "author_id": "1283646922406760448",
+            "created_at": "2022-06-24T18:05:33.000Z",
+            "entities": {
+                "urls": [
+                    {
+                        "display_url": "pic.twitter.com/EKNJmPRSDH",
+                        "end": 42,
+                        "expanded_url": "https://twitter.com/takanashikiara/status/1540395721194516480/photo/1",
+                        "media_key": "16_1540395710331228161",
+                        "start": 19,
+                        "url": "https://t.co/EKNJmPRSDH",
+                    }
+                ]
+            },
+            "id": "1540395721194516480",
+            "text": "Kikkerikiiiiiiìiii https://t.co/EKNJmPRSDH",
+        },
+        "includes": {
+            "media": [
+                {
+                    "height": 122,
+                    "media_key": "16_1540395710331228161",
+                    "preview_image_url": "https://pbs.twimg.com/tweet_video_thumb/FWCVvLfUEAEsMeQ.jpg",
+                    "public_metrics": {},
+                    "type": "animated_gif",
+                    "width": 220,
+                }
+            ],
+            "users": [
+                {
+                    "id": "1283646922406760448",
+                    "name": "Takanashi Kiara🐔",
+                    "username": "takanashikiara",
+                }
+            ],
+        },
+        "matching_rules": [{"id": "1539063278017585153", "tag": ""}],
+    }
+    pprint(a)
