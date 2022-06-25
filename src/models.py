fa42edc7726228df068c29b754e2696b10edc820
AoWangPhilly <aw3338@drexel.edu>
2022-06-25 12:26:40 -0400

    Create db models

---
+++ src/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from database import Base
+from sqlalchemy import Column, Integer, String
+
+class TwitterPost(Base):
+    __tablename__ = "twitter_posts"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    twitter_post_id = Column(Integer, primary_key=True, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False)
+    twitter_text = Column(String, nullable=False)
+    image_path = Column(String, nullable=False))
+    youtube_link = Column(String, nullable=False)
+
+    author_id = Column(Integer, ForeignKey(column="idols.author_id", ondelete="CASCADE"), nullable=False)
+    author = relationship("Idol")
+
+
+class Idol(Base):
+    __tablename__ = "idols"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    author_id = Column(Integer, primary_key=True, nullable=False)
+    username = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    icon_path = Column(String, nullable=False)
