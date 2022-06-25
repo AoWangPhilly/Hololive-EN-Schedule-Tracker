e24126466801748cf19fe397d10d26d8d6d86ab5
AoWangPhilly <aw3338@drexel.edu>
2022-06-25 12:13:37 -0400

    Create db

--- src/database.py
+++ src/database.py
@@ -1,9 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 from constants import SQLALCHEMY_DB_URL
 
 engine = create_engine(SQLALCHEMY_DB_URL)
+
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+print(f"{database_exists(engine.url)=}")
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
