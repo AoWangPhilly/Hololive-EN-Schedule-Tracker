2643c6cbecaf7f818732dd8497d64e2bf0740ef7
AoWangPhilly <aw3338@drexel.edu>
2022-06-28 13:13:42 -0400

    Include public metrics

--- src/constants.py
+++ src/constants.py
@@ -20,7 +20,9 @@ HOLOLIVE_EN_COUNCIL_TAGS = [
 
 SQLALCHEMY_DB_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
-TWITTER_FIELDS = "id,text,author_id,created_at,entities,referenced_tweets"
+TWITTER_FIELDS = (
+    "id,text,author_id,created_at,entities,referenced_tweets,public_metrics"
+)
 EXPANSIONS = "author_id,attachments.media_keys"
 MEDIA_FIELDS = "duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width,alt_text"
 USER_FIELDS = "username,name"
