dcf38bb608b861e45d56daeffef80b9fe2c2ad2c
AoWangPhilly <aw3338@drexel.edu>
2022-06-27 23:14:10 -0400

    Create constants for twitter stream filter

--- src/constants.py
+++ src/constants.py
@@ -19,3 +19,8 @@ HOLOLIVE_EN_COUNCIL_TAGS = [
 ]
 
 SQLALCHEMY_DB_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+
+TWITTER_FIELDS = "id,text,author_id,created_at,entities,referenced_tweets"
+EXPANSIONS = "author_id,attachments.media_keys"
+MEDIA_FIELDS = "duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width,alt_text"
+USER_FIELDS = "username,name"
