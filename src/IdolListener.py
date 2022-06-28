be07f3ce020af2f3a22523a251de89c0d9ef8ec9
AoWangPhilly <aw3338@drexel.edu>
2022-06-28 13:14:15 -0400

    Use response instead of raw data

--- src/IdolListener.py
+++ src/IdolListener.py
@@ -1,21 +1,11 @@
 import tweepy as tw
-from json import loads
-from util import store_twitter_post
-from pprint import pprint
+from utils import store_twitter_post
 
 
 class IdolListener(tw.StreamingClient):
     def on_connect(self):
         print("Connected!")
 
-    def on_data(self, raw_data):
-        tweet_data = loads(raw_data)
-
-        if tweet_data.get("data", None) is None:
-            print("WEIRD THING HAPPENED")
-            print("-" * 100)
-            print(tweet_data)
-
-        elif not tweet_data["data"].get("referenced_tweets"):
-            pprint(tweet_data)
-            store_twitter_post(tweet_data)
+    def on_response(self, response):
+        if not response.data.referenced_tweets:
+            store_twitter_post(response)
--- src/StreamingClientCleaner.py
+++ src/StreamingClientCleaner.py
@@ -1,45 +1,39 @@
+from pprint import pprint
 from typing import Any
+import tweepy as tw
+
+
+def extract_thumbnail_link_from_youtube_link(youtube_link: str) -> str:
+    start_index = youtube_link.rfind("/") + 1
+    video_id = youtube_link[start_index:]
+    return f"https://i.ytimg.com/vi/{video_id}/maxresdefault.jpg"
 
 
 class StreamingClientCleaner:
-    def clean_data(self, dirty_json: dict[str, Any]) -> dict[str, Any]:
-        cleaned_dict = {}
-        data = dirty_json.get("data")
-        includes = dirty_json.get("includes")
-        entities = data.get("entities")
+    @staticmethod
+    def reformat_response(response: tw.StreamResponse) -> dict[str, Any]:
+        output = {}
+        data, includes, _, _ = response
+        output["author_id"] = data.author_id
+        output["created_at"] = data.created_at
+        output["twitter_post_id"] = data.id
+        output["text"] = data.text
 
-        cleaned_dict["author_id"] = data["author_id"]
-        cleaned_dict["created_at"] = data["created_at"]
-        cleaned_dict["twitter_post_id"] = data["id"]
-        cleaned_dict["text"] = data["text"]
-
-        if entities is not None and "urls" in entities:
-            urls = entities["urls"][0]
-            if "unwound_url" in urls and urls["unwound_url"].startswith(
-                "https://www.youtube.com"
-            ):
-                cleaned_dict["youtube_link"] = urls["unwound_url"]
-
-                thumbnail_picture = self.__extract_thumbnail_link_from_youtube_link(
-                    cleaned_dict["youtube_link"]
-                )
-                cleaned_dict["image_path"] = thumbnail_picture
-
-        if includes is not None:
-            media = includes.get("media")
-            if media is not None:
-                content = includes["media"][0]
-                if "url" in content:
-                    cleaned_dict["image_path"] = content["url"]
-                elif "preview_image_url" in content:
-                    cleaned_dict["image_path"] = content["preview_image_url"]
-        return cleaned_dict
-
-    def __extract_thumbnail_link_from_youtube_link(self, youtube_link: str) -> str:
-        start_index = youtube_link.find("=") + 1
-        end_index = youtube_link.find("&")
-        if end_index != -1:
-            video_id = youtube_link[start_index:end_index]
-        else:
-            video_id = youtube_link[start_index:]
-        return f"https://i.ytimg.com/vi/{video_id}/maxresdefault.jpg"
+        if (
+            data.entities
+            and (url := data.entities["urls"][0]["expanded_url"])
+            and url.startswith("https://youtu.be")
+        ):
+            output["youtube_link"] = url
+            thumbnail_picture = extract_thumbnail_link_from_youtube_link(
+                output["youtube_link"]
+            )
+            output["image_path"] = thumbnail_picture
+        elif "media" in includes:
+            media = includes["media"][0]
+            if media.url:
+                output["image_path"] = media.url
+            elif media.preview_image_url:
+                output["image_path"] = media.preview_image_url
+        pprint(output)
+        return output
