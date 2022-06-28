7d0263fa36ff9cd0217d7809e891c3f31ce8b9f5
AoWangPhilly <aw3338@drexel.edu>
2022-06-27 23:27:11 -0400

    Abstract away the idol methods

--- src/util.py
+++ src/util.py
@@ -1,8 +1,3 @@
-from constants import (
-    HOLOLIVE_EN_COUNCIL_TAGS,
-    HOLOLIVE_EN_MYTH_TAGS,
-    HOLOLIVE_EN_VSINGER_TAGS,
-)
 from typing import Any, Optional
 import tweepy as tw
 from decouple import config
@@ -36,42 +31,6 @@ def connect_to_twitter() -> Optional[tw.API]:
     except Exception as e:
         raise e
     return api
-
-
-def generate_holo_en_rule() -> str:
-    hololive_tags = (
-        HOLOLIVE_EN_MYTH_TAGS + HOLOLIVE_EN_VSINGER_TAGS + HOLOLIVE_EN_COUNCIL_TAGS
-    )
-    hololive_tag = [f"from:{tag}" for tag in hololive_tags]
-    return " OR ".join(hololive_tag)
-
-
-def get_idol_info(screen_name: str) -> dict:
-    api = connect_to_twitter()
-    user = api.get_user(screen_name=screen_name)
-    return {
-        "author_id": user.id,
-        "username": screen_name,
-        "name": user.name,
-        "icon_path": user.profile_image_url_https,
-    }
-
-
-def store_all_idol_info_to_db() -> None:
-    IDOLS = (
-        HOLOLIVE_EN_COUNCIL_TAGS + HOLOLIVE_EN_MYTH_TAGS + HOLOLIVE_EN_VSINGER_TAGS,
-    )[0]
-
-    db_gen = get_db()
-    db = next(db_gen)
-    for idol_screen_name in IDOLS:
-        print(idol_screen_name)
-        idol_info = get_idol_info(idol_screen_name)
-        idol = models.Idol(**idol_info)
-        db.add(idol)
-        db.commit()
-        db.refresh(idol)
-        print(idol)
 
 
 def store_twitter_post(data: dict[str, Any]) -> None:
