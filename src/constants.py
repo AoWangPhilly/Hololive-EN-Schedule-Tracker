83e0a94aae74d95ed38a8b45f2d7120b78cbb859
AoWangPhilly <aw3338@drexel.edu>
2022-07-01 22:05:16 -0400

    Add in youtube ids

--- src/constants.py
+++ src/constants.py
@@ -18,6 +18,23 @@ HOLOLIVE_EN_COUNCIL_TAGS = [
     "hakosbaelz",
 ]
 
+HOLOLIVE_EN_MYTH_YOUTUBE_ID = [
+    "UCoSrY_IQQVpmIRZ9Xf-y93g",  # Gawr Gura
+    "UCHsx4Hqa-1ORjQTh9TYDhww",  # Takanashi Kiara
+    "UCL_qhgtOy0dy1Agp8vkySQg",  # Mori Calliope
+    "UCMwGHR0BTZuLsmjY_NT5Pwg",  # Ninomae Ina'nis
+    "UCyl1z3jo3XHR1riLFKG5UAg",  # Watson Amelia
+]
+
+HOLOLIVE_EN_COUNCIL_YOUTUBE_ID = [
+    "UC3n5uGu18FoCy23ggWWp8tA",  # Nanashi Mumei
+    "UCO_aKKYxn4tvrqPjcTzZ6EQ",  # Ceres Fauna
+    "UCgmPnx-EEeOrZSg5Tiw7ZRQ",  # Hakos Baelz
+    "UCmbs8T6MWqUHP1tIQvSgKrg",  # Ouro Kronii
+    "UCsUj0dszADCGbF3gNrQEuSQ",  # Tsukumo Sana
+]
+HOLOLIVE_EN_VSINGER_YOUTUBE_ID = ["UC8rcEBzJSleTkf_-agPM20g"]  # IRyS
+
 SQLALCHEMY_DB_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 TWITTER_FIELDS = (
