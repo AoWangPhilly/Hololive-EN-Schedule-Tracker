fdfb3f92116f052c15d0f89055f46ecd8223eba2
AoWangPhilly <aw3338@drexel.edu>
2022-07-01 23:48:42 -0400

    Abstract away constant to another package

---
+++ src/youtube/utils/constants.py
@@ -0,0 +1,16 @@
+HOLOLIVE_EN_MYTH_YOUTUBE_ID = [
+    "UCoSrY_IQQVpmIRZ9Xf-y93g",  # Gawr Gura
+    "UCHsx4Hqa-1ORjQTh9TYDhww",  # Takanashi Kiara
+    "UCL_qhgtOy0dy1Agp8vkySQg",  # Mori Calliope
+    "UCMwGHR0BTZuLsmjY_NT5Pwg",  # Ninomae Ina'nis
+    "UCyl1z3jo3XHR1riLFKG5UAg",  # Watson Amelia
+]
+
+HOLOLIVE_EN_COUNCIL_YOUTUBE_ID = [
+    "UC3n5uGu18FoCy23ggWWp8tA",  # Nanashi Mumei
+    "UCO_aKKYxn4tvrqPjcTzZ6EQ",  # Ceres Fauna
+    "UCgmPnx-EEeOrZSg5Tiw7ZRQ",  # Hakos Baelz
+    "UCmbs8T6MWqUHP1tIQvSgKrg",  # Ouro Kronii
+    "UCsUj0dszADCGbF3gNrQEuSQ",  # Tsukumo Sana
+]
+HOLOLIVE_EN_VSINGER_YOUTUBE_ID = ["UC8rcEBzJSleTkf_-agPM20g"]  # IRyS
