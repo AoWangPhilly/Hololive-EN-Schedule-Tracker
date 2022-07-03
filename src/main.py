c04094bda75461bd9dc2e79ded17fdbde3c1291d
AoWangPhilly <aw3338@drexel.edu>
2022-07-03 17:06:21 -0400

    Change name to twitter_model

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from src.database import engine
-import src.models as models
+import src.twitter.models as twitter_models
 from src.twitter import IdolListener
 
 if __name__ == "__main__":
-    models.Base.metadata.create_all(engine)
+    twitter_models.Base.metadata.create_all(engine)
+
     IdolListener.run()
