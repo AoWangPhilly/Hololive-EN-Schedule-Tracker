4f40d6b647ee0c7bcebf43cc3c0ff58cb30c2232
AoWangPhilly <aw3338@drexel.edu>
2022-07-03 17:07:22 -0400

    Run schduled program daily

--- src/youtube/main.py
+++ src/youtube/main.py
@@ -1,23 +1,16 @@
 from decouple import config
+import schedule
+import time
 
-from src.youtube.YouTubeStats import YouTubeStats
-from src.youtube.utils.constants import (
-    HOLOLIVE_EN_MYTH_YOUTUBE_ID,
-    HOLOLIVE_EN_COUNCIL_YOUTUBE_ID,
-    HOLOLIVE_EN_VSINGER_YOUTUBE_ID,
-)
-
-API_KEY = config("YOUTUBE_API_KEY")
-
+from src.database import engine
+from src.youtube.YouTubeStats import run
+import src.youtube.models as youtube_models
 
 if __name__ == "__main__":
-    yt_ids = (
-        HOLOLIVE_EN_MYTH_YOUTUBE_ID
-        + HOLOLIVE_EN_COUNCIL_YOUTUBE_ID
-        + HOLOLIVE_EN_VSINGER_YOUTUBE_ID
-    )
-    for yt_id in yt_ids:
-        yt = YouTubeStats(api_key=API_KEY, channel_id=yt_id)
+    youtube_models.Base.metadata.create_all(engine)
 
-        yt.get_channel_statistics()
-        print(yt.channel_statistics)
+    schedule.every().day.at("17:00").do(run)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
