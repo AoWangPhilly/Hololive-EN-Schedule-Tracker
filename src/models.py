e1270a1446c9e04730635105c2a380d88c67ecd6
AoWangPhilly <aw3338@drexel.edu>
2022-07-08 21:32:12 -0400

    Move models into single file

--- src/models.py
+++ src/models.py
@@ -1,11 +1,43 @@
 """SQLAlchemy data models that represent a SQL table in the Hololive db, where the attributes of a model translate to columns in a table
 """
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql.schema import UniqueConstraint
+from sqlalchemy.sql.schema import ForeignKey, UniqueConstraint
 from sqlalchemy.sql.sqltypes import DateTime, TIMESTAMP
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String, BigInteger
 from sqlalchemy.sql import func
+
+
 from src.database import Base
+
+
+class Idol(Base):
+    __tablename__ = "idols"
+    __table_args__ = (UniqueConstraint("author_id"),)
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    author_id = Column(BigInteger, nullable=False)
+    username = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    icon_path = Column(String, nullable=False)
+
+
+class TwitterPost(Base):
+    __tablename__ = "twitter_posts"
+    __table_args__ = (UniqueConstraint("twitter_post_id"),)
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    twitter_post_id = Column(BigInteger, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False)
+    text = Column(String, nullable=False)
+    image_path = Column(String)
+    youtube_link = Column(String)
+
+    author_id = Column(
+        BigInteger,
+        ForeignKey(column="idols.author_id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    author = relationship("Idol")
 
 
 class YouTubeIdol(Base):
@@ -22,7 +54,6 @@ class YouTubeIdol(Base):
 
 class YouTubeStatistic(Base):
     __tablename__ = "youtube_statistics"
-    __table_args__ = (UniqueConstraint("youtube_id"),)
 
     id = Column(Integer, primary_key=True, nullable=False)
     youtube_id = Column(
