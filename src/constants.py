85eb2dcf35dc47e6acf6defa9c314c277384db71
AoWangPhilly <aw3338@drexel.edu>
2022-07-08 21:15:14 -0400

    Remove youtube utils and move constants

--- src/constants.py
+++ src/constants.py
@@ -27,3 +27,20 @@ TWITTER_FIELDS = (
 EXPANSIONS = "author_id,attachments.media_keys"
 MEDIA_FIELDS = "duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width,alt_text"
 USER_FIELDS = "username,name"
+
+HOLOLIVE_EN_MYTH_YOUTUBE_ID = [
+    "UCoSrY_IQQVpmIRZ9Xf-y93g",  # Gawr Gura
+    "UCHsx4Hqa-1ORjQTh9TYDhww",  # Takanashi Kiara
+    "UCL_qhgtOy0dy1Agp8vkySQg",  # Mori Calliope
+    "UCMwGHR0BTZuLsmjY_NT5Pwg",  # Ninomae Ina'nis
+    "UCyl1z3jo3XHR1riLFKG5UAg",  # Watson Amelia
+]
+
+HOLOLIVE_EN_COUNCIL_YOUTUBE_ID = [
+    "UC3n5uGu18FoCy23ggWWp8tA",  # Nanashi Mumei
+    "UCO_aKKYxn4tvrqPjcTzZ6EQ",  # Ceres Fauna
+    "UCgmPnx-EEeOrZSg5Tiw7ZRQ",  # Hakos Baelz
+    "UCmbs8T6MWqUHP1tIQvSgKrg",  # Ouro Kronii
+    "UCsUj0dszADCGbF3gNrQEuSQ",  # Tsukumo Sana
+]
+HOLOLIVE_EN_VSINGER_YOUTUBE_ID = ["UC8rcEBzJSleTkf_-agPM20g"]  # IRyS
