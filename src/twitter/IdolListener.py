f1078e7eb7c5b2111c4d905d24ad8c749a980654
AoWangPhilly <aw3338@drexel.edu>
2022-07-08 22:49:42 -0400

    Fix imports

--- src/twitter/IdolListener.py
+++ src/twitter/IdolListener.py
@@ -1,9 +1,8 @@
 import tweepy as tw
 from decouple import config
 
-from src.twitter.utils.util import store_twitter_post
-from src.twitter.Idol import generate_holo_en_rule
-from src.twitter.utils.constants import (
+from src.twitter.Idol import Idol
+from src.utils.constants import (
     TWITTER_FIELDS,
     EXPANSIONS,
     MEDIA_FIELDS,
@@ -17,11 +16,11 @@ class IdolListener(tw.StreamingClient):
 
     def on_response(self, response: tw.StreamResponse):
         if response.data and not response.data.referenced_tweets:
-            store_twitter_post(response)
+            Idol.store_twitter_post(response)
 
 
 def run():
-    rule = generate_holo_en_rule()
+    rule = Idol.generate_holo_en_rule()
     streaming_client = IdolListener(config("BEARER_TOKEN"))
     streaming_client.add_rules(tw.StreamRule(rule))
 
