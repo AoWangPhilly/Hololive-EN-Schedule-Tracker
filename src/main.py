c12a3d2f7d6947a8cda0bf7bb57550fb6e0ae7a4
AoWangPhilly <aw3338@drexel.edu>
2022-07-08 22:49:19 -0400

    Run two processes simultaneously

--- src/main.py
+++ src/main.py
@@ -1,8 +1,30 @@
+import schedule
+import time
+from multiprocessing import Process
+
 from src.database import engine
-import src.twitter.models as twitter_models
+import src.models as models
 from src.twitter import IdolListener
+import src.youtube.YouTubeRunner as YouTubeRunner
+
+
+def youtube_process():
+    schedule.every().day.at("12:00").do(YouTubeRunner.run)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
+
+
+def main():
+    models.Base.metadata.create_all(engine)
+
+    p1 = Process(target=youtube_process)
+    p2 = Process(target=IdolListener.run)
+
+    p1.start()
+    p2.start()
+
 
 if __name__ == "__main__":
-    twitter_models.Base.metadata.create_all(engine)
-
-    IdolListener.run()
+    main()
